d1cbf6a0610955daf49b96af78af34aef271aa99
Beastplay1 <91196171+Beastplay1@users.noreply.github.com>
2021-10-06 10:52:56 +0400

    Update index3.php

--- index3.php
+++ index3.php
@@ -1,32 +1,42 @@
 <?php
 
-$arr = [-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9,10];
+#$arr = [-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9,10];
 
-$len = count($arr);
+#$len = count($arr);
 
-$num = 0;
+#$num = 0;
 
-for ($i=0;$i < $len;$i++) {
-if($arr[$i] < 0) {
-   $num++;
-}
-}
+#for ($i=0;$i < $len;$i++) {
+#if($arr[$i] < 0) {
+ #  $num++;
+#}
+#}
 
-echo $num;
+#echo $num;
 
-for($i=0;$i < $len;$i++) {
-if($arr[$i] > 0 && $arr[$i] % 2 == 0) {
-echo $i."<br>";
-}
-}
+#for($i=0;$i < $len;$i++) {
+#if($arr[$i] > 0 && $arr[$i] % 2 == 0) {
+#echo $i."<br>";
+#}
+#}
 
-$a = 0;
+#$a = 0;
 
-for($i=0;$i < $len;$i++) {
-   if($arr[$i] > 0) {
-       $a += $arr[$i];
-}
+#for($i=0;$i < $len;$i++) {
+   #if($arr[$i] > 0) {
+      # $a += $arr[$i];
+#}
 
-echo $a;
+#echo $a;
+
+$arr = [];
+
+
+  for($i=1;$i < 101;$i++) {
+    $arr[] = $i;
+    $arr[$i] = rand(1,100);
+    }
+
+print_r($arr);
 
 ?>
