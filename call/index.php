cf1b01583af6e546d276e37710669d58a33ef1f2
Beastplay1 <91196171+Beastplay1@users.noreply.github.com>
2021-11-22 14:26:04 +0400

    Add files via upload

---
+++ call/index.php
@@ -0,0 +1,85 @@
+<?php 
+class myclass {
+	public $name;
+	public $surname;
+	public $age;
+	public function __invoke($x){
+
+		echo $x+$x;
+	}
+	public function __clone(){
+		$this->name = "Arman";
+	}
+	public function __call($a,$b){
+		$start = substr($a,0,3 );
+		$end = substr($a,3);
+		if($start === "set"){
+			if(property_exists($this,$end)){
+				$this->$end = $b[0];
+			}
+		    else{
+			   echo 'mer classi mej tenc popoxakan chka';
+		    }
+		}
+		elseif($start==="get"){
+			if(property_exists($this,$end)){
+				return $this->$end;
+			}
+			 else{
+			   echo 'mer classi mej tenc popoxakan chka';
+		    }
+		}
+		else{
+			echo 'mer classi mej tenc funkcia chka';
+		}
+
+	}
+}
+$obj = new myclass;
+// $obj(25);
+$obj1 = new myclass;
+$obj2 = clone $obj1;  
+// print_r($obj1);
+$obj-> setname("Arman");
+$obj-> setsurname("Hakobyan");
+$obj-> setage(17);
+echo $obj-> getage();
+echo $obj-> getname();
+echo $obj-> getsurname();
+// echo "<pre>";
+print_r($obj);
+
+// __construct();
+// __destruct();
+// __autoload();
+// __tostring();
+// __set();
+// __get();
+// __isset();
+// __unset();
+// __call();
+// __callStatic();
+// __invoke();
+// __sleep();
+// __wakeup();
+//__clone();
+trait a{
+	public function getx(){
+		return $this->x;
+	}
+	public function show(){
+		echo  $this->x;
+	}
+}
+class newclass{
+	public $x;
+	use a;//ogtagorcelu hamar kanchum enq trait i anun@,u inch ka trait i mej includea anum 
+}
+
+
+//avtostayna enq sarqelu 
+//bernatarneri hamar 
+//mardatarneri hamar
+class parking{
+	
+}
